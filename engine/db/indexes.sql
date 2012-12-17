fa514815ff771826d81a27e14fe70dd9bd722abd
einar <einar@iis.se>
2012-12-17 10:00:21 +0100

    Update engine/db/indexes.sql
    Let's see if it works to update from github.com... ;) Just disabled the default index for DOMAINS since the code doesnt really use it.

--- engine/db/indexes.sql
+++ engine/db/indexes.sql
@@ -3,4 +3,5 @@ create index queue_domain on queue (domain(15));
 create index queue_priority on queue (priority);
 create index tests_begin on tests (begin);
 create index domain_delegation_history on delegation_history (domain(15));
-create index domains_domain on domains (domain(15));
+#create index domains_domain on domains (domain(15));
+### The index on table DOMAINS is only useful if you USE this table... default doesnt so thus it's disabled by default.
